201d5c3a843a77c068528aa3cf69186f4dd5306a
brian thomas <brian-thomas@comcast.net>
2016-08-17 21:34:05 -0400

    Filter on the word group to the top word reducing the overall number of rows

--- filter_ngrams.R
+++ filter_ngrams.R
@@ -1,20 +1,20 @@
 # filter_ngrams.R
-# This script filters down the results by choosing bigrams/trigrams that occur more than 1 time
+# This script filters down the results by choosing bigrams/trigrams with the highest count
 
 rm(list=ls())
+library(dplyr)
+library(data.table)
 
 setwd("/home/bthomas/Github/final_shiny_code")
 trigrams <- readRDS("flt_trigrams.Rda")
-trigrams2 <- as.data.frame(trigrams)
-trigrams3 <- trigrams2 %>% group_by(W1, W2) %>% top_n(1, Freq)
-saveRDS(trigrams3, "flt_trigrams.Rda")
+trigrams <- as.data.frame(trigrams)
+trigrams <- trigrams %>% group_by(W1, W2) %>% top_n(1, Freq)
+saveRDS(trigrams, "flt_trigrams.Rda")
 
 setwd("/home/bthomas/Github/final_shiny_code")
 bigrams <- readRDS("bigrams.Rda")
-bigrams2 <- as.data.frame(bigrams)
-bigrams3 <- bigrams2 %>% group_by(W1) %>% top_n(1, Freq)
-setkey(bigrams3, W1)
-object.size(bigrams3)
-saveRDS(bigrams3, "flt_bigrams.Rda")
+bigrams <- as.data.frame(bigrams)
+bigrams <- bigrams %>% group_by(W1) %>% top_n(1, Freq)
+saveRDS(bigrams, "flt_bigrams.Rda")
 
 
